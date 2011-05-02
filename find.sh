57429e456a8277e0673e0318396932c8dccdd2ae
René Klose <rene.klose@gmail.com>
2011-05-02 04:59:12 -0700

    Edited find.sh via GitHub

--- find.sh
+++ find.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+FILE=$"filename here"
 wget somewebpage.com
 D1=$(cat index.html | grep '<a href' | grep Camelot | sed -e's/.*\(http:.*\sion\).*/\1/')
 wget $D1
-cat cam* | grep '<a href' | grep https://rapidshare |  sed -e's/.*\(https:.*\rar\).*/\1/' | grep 720
+cat $FILE* | grep '<a href' | grep https://rapidshare |  sed -e's/.*\(https:.*\rar\).*/\1/' | grep 720
 echo "habe fertig"
 
