5d20e7260b6b3f88694aabe2bf046320fda12260
René Klose <rene.klose@gmail.com>
2011-05-02 04:57:47 -0700

    Edited find.sh via GitHub

--- find.sh
+++ find.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget www.oneddl.com
+wget somewebpage.com
 D1=$(cat index.html | grep '<a href' | grep Camelot | sed -e's/.*\(http:.*\sion\).*/\1/')
 wget $D1
 cat cam* | grep '<a href' | grep https://rapidshare |  sed -e's/.*\(https:.*\rar\).*/\1/' | grep 720
